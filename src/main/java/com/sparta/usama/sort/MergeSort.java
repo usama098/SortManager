5c3d016d4683efea29ae86a0d6e99f54758c8a8e
Usama <usamanavid098@gmail.com>
2022-04-22 16:13:24 +0100

    8th push, now the merge sort works

--- src/main/java/com/sparta/usama/sort/MergeSort.java
+++ src/main/java/com/sparta/usama/sort/MergeSort.java
@@ -1,24 +1,17 @@
 package com.sparta.usama.sort;
 
-import static java.util.Arrays.mergeSort;
 
 public class MergeSort implements Sorter {
 
-//    @Override
-//    public int[] sortArray(int[] inputArray) {
-//
-//        if (inputArray == null || inputArray.length == 0) {
-//            throw new IllegalArgumentException("nothing here, look away young one");
-//        }
-//
-//        if (isArraySorted(inputArray)) {
-//            throw new IllegalArgumentException("bro its already sorted dawg, don't stress");
-//        }
-//
-//        return mergeSortArray(inputArray, 0, inputArray.length - 1);
-//    }
-    private int[] mergeArray(int[]inputArray){
-        int n = inputArray.length;
+
+    @Override
+    public int[] sortArray(int[] inputArray) {
+        mergeArray(inputArray, inputArray.length);
+        return inputArray;
+    }
+
+    private int[] mergeArray(int[]inputArray, int n){
+        n = inputArray.length;
         if (n < 2) {
             return inputArray;
         }
@@ -56,72 +49,4 @@ public class MergeSort implements Sorter {
         }
 
     }
-
-//
-//    private int[] mergeArray(int[] numberArray, int beginIndex, int midIndex,int endIndex){
-//
-//        int leftLength = midIndex - beginIndex + 1;
-//        int rightLength = endIndex - midIndex;
-//        int[] leftArray = new int[leftLength];
-//        int[] rightArray = new int[rightLength];
-//
-//        if (leftLength >= 0) {
-//            System.arraycopy(numberArray, beginIndex, leftArray, 0, leftLength);
-//        }
-//
-//        if (rightLength >= 0) {
-//            System.arraycopy(numberArray, midIndex + 1, rightArray, 0, rightLength);
-//        }
-//
-//        int leftIndex = 0, rightIndex = 0;
-//        int count = beginIndex;
-//        while (leftIndex < leftLength && rightIndex < rightLength) {
-//            if (leftArray[leftIndex] <= rightArray[rightIndex]) {
-//                numberArray[count] = leftArray[leftIndex];
-//                leftIndex++;
-//            } else {
-//                numberArray[count] = rightArray[rightIndex];
-//                rightIndex++;
-//            }
-//            count++;
-//        }
-//        while (leftIndex < leftLength) {
-//            numberArray[count -1] = leftArray[leftIndex];
-//            rightIndex++;
-//            count++;
-//        }
-//        return numberArray;
-//
-//
-//    }
-//
-//    private boolean isArraySorted(int[] numberArray) {
-//        for (int i = 0; i < numberArray.length; i++) {
-//            if (numberArray[i] > numberArray[i+1]) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    private int[] combineTwoArrays(int[] numberArray1, int[] numberArray2) {
-//        int[] arraysCombined = new int[numberArray1.length + numberArray2.length];
-//        System.arraycopy(numberArray1, 0, arraysCombined, 0, numberArray1.length);
-//        System.arraycopy(numberArray2, 0, arraysCombined, numberArray1.length, numberArray2.length);
-//
-//        return arraysCombined;
-//
-//    }
-//
-//    private int[] mergeSortArray(int[] numberArray, int beginIndex, int endIndex) {
-//        if (beginIndex < endIndex) {
-//            int midIndex = (beginIndex + endIndex) / 2;
-//            numberArray = mergeSortArray(numberArray, beginIndex, midIndex);
-//            numberArray = mergeSortArray(numberArray, midIndex + 1, endIndex);
-//
-//            return mergeArray(numberArray, beginIndex, midIndex, endIndex);
-//        }
-//        return numberArray;
-//    }
-
 }
