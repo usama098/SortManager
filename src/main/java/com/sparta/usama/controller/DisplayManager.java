8c5511c91d0a93d198854ffe011fa5b65f8b5ded
Usama <usamanavid098@gmail.com>
2022-04-22 17:04:27 +0100

    9th push,it's broken again

--- src/main/java/com/sparta/usama/controller/DisplayManager.java
+++ src/main/java/com/sparta/usama/controller/DisplayManager.java
@@ -5,21 +5,29 @@ import com.sparta.usama.sort.*;
 
 public class DisplayManager {
     public static void start() {
-        System.out.println("Welcome to Sort Manager. \nSelect the size of the array below");
-        SortManager.start();
+        System.out.println("Welcome to Sort Manager. \n\n\tSelect the size of the array below");
+        SortManager.length();
+        System.out.println("\tSelect the type of the sorting algorithm options are: Bubble or Merge");
+        SortManager.sorting();
+
         int[] newArray = ArrayGenerator.generateIntArray(SortManager.inputLength);
-        DisplayManager.showBubbleArray(newArray);
-        Sorter sBubbleArray = SortFactory.getSort("merge");
-        sBubbleArray.sortArray(newArray);
-        DisplayManager.showBubbleArray(newArray);
+        DisplayManager.showArray(newArray, false);
+        Sorter sorter = SortFactory.getSort(SortManager.sortAlgorithm);
+        sorter.sortArray(newArray);
+        DisplayManager.showArray(newArray, true);
 
 
 
     }
 
-    public static void showBubbleArray(int[] showNewArray) {
-        Printer.printString("The generated random array is the following: ");
-        Printer.printIntArray(showNewArray);
+    public static void showArray(int[] showNewArray, boolean sorted) {
+        if (sorted == false) {
+            Printer.printString("The random generated array is the following: ");
+            Printer.printIntArray(showNewArray);
+        } else {
+            Printer.printString("The sorted array is the following: ");
+            Printer.printIntArray(showNewArray);
+        }
     }
 
 }
--- src/main/java/com/sparta/usama/controller/SortManager.java
+++ src/main/java/com/sparta/usama/controller/SortManager.java
@@ -1,15 +1,44 @@
 package com.sparta.usama.controller;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class SortManager {
 
     public static int inputLength = 0;
+    public static String sortAlgorithm = "";
 
-    public static void start() {
+    public static void length() {
 
-        Scanner myObject = new Scanner(System.in);
-        inputLength = myObject.nextInt();
+        boolean validInput = false;
 
+        while (validInput == false) {
+            Scanner myObject = new Scanner(System.in);
+            try {
+                inputLength = myObject.nextInt();
+                validInput = true;
+            } catch (InputMismatchException msg) {
+                System.out.println("Input Mismatch Exception has occurred " + msg.getMessage());
+            }
+        }
+    }
+    public static void sorting(){
+        boolean validInput = false;
+
+        while(validInput == false) {
+            Scanner myObject = new Scanner(System.in);
+            try {
+                sortAlgorithm = myObject.nextLine();
+                sortAlgorithm = sortAlgorithm.toLowerCase();
+                if (sortAlgorithm == "bubble" || sortAlgorithm == "merge"){
+                    validInput = true;
+                } else{
+                    validInput = false;
+                }
+            } catch (InputMismatchException msg) {
+                System.out.println("Input Mismatch Exception has occurred " + msg.getMessage());
+            }
+        }
     }
 }
